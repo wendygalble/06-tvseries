04006c601f8d67505d11ad809238b683c08f1a80
wendy <wendygalble@gmail.com>
2023-07-08 13:21:45 -0600

    muestra img2

--- src/components/SerieCard.jsx
+++ src/components/SerieCard.jsx
@@ -1,15 +1,16 @@
-const SerieCard = ({ id, name, image }) => {
+const SerieCard = ({ id, name, image, summary }) => {
   return (
     <div className='col-md-3 my-3 d-flex align-items-stretch'>
       <div className='card'>
         <a href={`/serie/${id}`}>
           <div className='image-container'>
-            <img className='serie-image' src={`https://static.tvmaze.com/uploads/images/original_untouched/81/202627.jpg ${image}`} alt={name} />
+            <img className='serie-image' src={`${image.original}`} />
           </div>
         </a>
         <div className='card-body d-flex flex-column justify-content-between'>
           <div>
             <h5 className='card-name'>{name}</h5>
+            <p className='card-name'>{summary}</p>
           </div>
         </div>
       </div>
