c4f0122e43eabea81d1c2f383694f123a097886b
wendy <wendygalble@gmail.com>
2023-07-09 16:40:24 -0600

    useParams en serie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
 const Header = () => {
   return (
-    <>
-      <nav className='navbar bg-body-tertiary'>
-        <div className='container-fluid'>
-          <span className='navbar-brand mb-0 h1'>BUSCADOR DE SERIES DE TV</span>
-        </div>
-      </nav>
-    </>
+    <div className='container'>
+      <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
+        <NavLink
+          to='/'
+          className='d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none'
+        >
+          <svg className='bi me-2' width={40} height={32}>
+            <use xlinkHref='#bootstrap' />
+          </svg>
+          <span className='fs-4'>Buscador de seriesTV</span>
+        </NavLink>
+      </header>
+    </div>
+
   )
 }
 
--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 const Serie = () => {
+  const [serie, setSerie] = useState([])
+  const serieId = useParams().id
+
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${serieId}`)
+      .then((response) => {
+        return response.json()
+      }).then((series) => {
+        setSerie(series)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }, [serieId])
+
   return (
-    <div>Serie</div>
+    <div>
+      <p>El ID de la serie es {serieId}</p>
+      <h2>NOMBRE: {serie.name}</h2>
+
+    </div>
   )
 }
 
