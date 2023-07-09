fdd2a88f688155a9915b1f875bad258680b2e6d3
wendy <wendygalble@gmail.com>
2023-07-09 01:23:20 -0600

    SerieCard con Link de react-router-dom

--- src/components/SerieCard.jsx
+++ src/components/SerieCard.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
+
 const SerieCard = ({ id, name, image, summary, officialSite }) => {
   return (
     <div className='col-md-4 my-4 d-flex align-items-stretch'>
       <div className='card'>
-        <a href={`/serie/${id}`}>
+        <Link to={`/serie/${id}`}>
           <div className='image-container'>
             <img className='serie-image' src={`${image.original}`} />
           </div>
-        </a>
+        </Link>
         <div className='card-body d-flex flex-column justify-content-between'>
           <div>
             <h1 className='card-name'>{name}</h1>
@@ -14,7 +16,7 @@ const SerieCard = ({ id, name, image, summary, officialSite }) => {
           </div>
 
           <div className='mx-auto mt-3'>
-            <a className='ver' href={officialSite}>WATCH NOW</a>
+            <Link className='ver' to={officialSite}>WATCH NOW</Link>
           </div>
         </div>
       </div>
