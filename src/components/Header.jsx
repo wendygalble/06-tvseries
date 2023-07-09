9e6a1e8a29ef5ec4b9ef7c9c8c8f4933ac489ace
wendy <wendygalble@gmail.com>
2023-07-08 22:20:44 -0600

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar bg-body-tertiary'>
+        <div className='container-fluid'>
+          <span className='navbar-brand mb-0 h1'>BUSCADOR DE SERIES DE TV</span>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
