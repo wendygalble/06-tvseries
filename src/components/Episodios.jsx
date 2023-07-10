fb0df187b5103e87f061fe5e5a27015ed9bf0e97
wendy <wendygalble@gmail.com>
2023-07-09 23:41:11 -0600

    episodios en tabla

--- src/components/Episodios.jsx
+++ src/components/Episodios.jsx
@@ -1,9 +1,24 @@
-const Episodios = ({ name, season }) => {
+const Episodios = ({ name, season, number, summary }) => {
   return (
-    <div>
-      <p>Nombre del episodio: {name}</p>
-      <p>Temporada: {season}</p>
-    </div>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th scope='col'>No. Episodio</th>
+          <th scope='col'>Temporada</th>
+          <th scope='col'>Nombre</th>
+          <th scope='col'>Resumen del episodio</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope='row'>{number}</th>
+          <td>{season}</td>
+          <td>{name}</td>
+          <td>{summary}</td>
+        </tr>
+      </tbody>
+    </table>
+
   )
 }
 
